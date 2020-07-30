9711022ff87bfb9b0eb64bfc6c52597438e05395
unknown <jueeli97@gmail.com>
2020-07-30 22:41:09 +0530

    Operators in JavaScript
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// OPERATORS
+
+// Addition
+
+// Number
+let numValue1 = 10;
+let numValue2 = 20;
+console.log(numValue1 + numValue2); // 30
+
+// substraction
+let numValue3 = numValue1 - numValue2;
+console.log(numValue3); // -10
+
+// multipication
+numValue3 = numValue1 * numValue2;
+console.log(numValue3);// 200
+
+//division
+numValue3= numValue1/ numValue2;
+console.log(numValue3);//0.5
+
+//  To find Remainder
+numValue3 = numValue1 % numValue2;
+console.log(numValue3); // 10
+
+
+// Mathematical operations
+let numValue4 = 30;
+let result = numValue1 + numValue2 * numValue4;
+console.log(result); // 610
+
+
+ result = (numValue1 + numValue2) * numValue4;
+console.log(result);// 900
+
+result = numValue1 *(numValue2/numValue1) *(numValue4 + numValue1);
+console.log(result);// 800
+
+
+// Power of Numbers
+result = Math.pow(numValue1,2);
+console.log(result); // 100
+
+result = numValue2 ** 3;
+console.log(result); // 8000
+
